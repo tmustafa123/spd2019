713d1f1c141cd00d50195e05bef168f98fb710e3
Tajammul.Mustafa <Tajammul.Mustafa@e-mundo.de>
2019-03-17 21:32:49 +0100

    fix name typos

--- tokenring.c
+++ tokenring.c
@@ -4,9 +4,9 @@ int main(int argc, char **argv)
 {
 
     MPI_Init(&argc, &argv);
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     int token;
     if (world_rank != 0)
     {
